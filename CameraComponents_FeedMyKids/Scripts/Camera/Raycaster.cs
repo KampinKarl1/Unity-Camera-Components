03487376901a02acdc3e0a6de0b19710cd217caf
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2023-04-24 15:04:49 +0200

    Update Raycaster.cs

--- CameraComponents_FeedMyKids/Scripts/Camera/Raycaster.cs
+++ CameraComponents_FeedMyKids/Scripts/Camera/Raycaster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CustCameraComponents{
+namespace CustomCameraComponents{
 
 public class Raycaster : MonoBehaviour
 {
